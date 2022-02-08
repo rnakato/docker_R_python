37331d262ba87f0d096acec0c0219a3f80e9d0c1
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-02-08 20:16:59 +0900

    do not change locage to JP

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-for tag in 2021.11 latest #2020.12 #pytorch-1.5-cuda10.1-cudnn7-devel
+for tag in 2022.02  latest #2021.11 #2020.12 #pytorch-1.5-cuda10.1-cudnn7-devel
 do
     docker build -f Dockerfile.GPU -t rnakato/r_python_gpu:$tag . #--no-cache
     docker push rnakato/r_python_gpu:$tag
