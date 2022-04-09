daa985fd508d6da24aa9e02a3a9953e4fb0d73fc
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-04-09 16:56:23 +0900

    mv pip to conda-forge

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
-for tag in 2022.02  latest #2021.11 #2020.12 #pytorch-1.5-cuda10.1-cudnn7-devel
+for tag in #2022.02 latest #2021.11 #2020.12 #pytorch-1.5-cuda10.1-cudnn7-devel
 do
     docker build -f Dockerfile.GPU -t rnakato/r_python_gpu:$tag . #--no-cache
     docker push rnakato/r_python_gpu:$tag
 done
 
-
-
-#for tag in #r40u18 latest
-#do
-#    docker build -f Dockerfile.R4.0.Ubuntu18.04 -t rnakato/r_python:$tag . #--no-cache
-#    docker push rnakato/r_python:$tag
-#done
+for tag in 20.04 latest
+do
+    docker build -f Dockerfile.20.04 -t rnakato/r_python:$tag .
+    docker push rnakato/r_python:$tag
+done
