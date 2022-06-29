9a46dc9f2e1254bdfcd8fde831a6ae0cd28f9924
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-06-29 14:03:52 +0900

    modify MACS2

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-for tag in 11.3.1-cudnn8-runtime-ubuntu20.04 latest
+for tag in #11.3.1-cudnn8-runtime-ubuntu20.04 latest
 do
     docker build -f Dockerfile.GPU -t rnakato/r_python_gpu:$tag . #--no-cache
     docker push rnakato/r_python_gpu:$tag
 done
 
-for tag in #20.04 latest
+for tag in 20.04 latest
 do
     docker build -f Dockerfile.20.04 -t rnakato/r_python:$tag .
     docker push rnakato/r_python:$tag
