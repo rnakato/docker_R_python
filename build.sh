cb3c762a083ec25f53b68a289d30c9042f545849
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-06-29 12:24:28 +0900

    update by hadolint

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-for tag in #2022.02 latest #2021.11 #2020.12 #pytorch-1.5-cuda10.1-cudnn7-devel
+for tag in 11.3.1-cudnn8-runtime-ubuntu20.04 latest
 do
     docker build -f Dockerfile.GPU -t rnakato/r_python_gpu:$tag . #--no-cache
     docker push rnakato/r_python_gpu:$tag
 done
 
-for tag in 20.04 latest
+for tag in #20.04 latest
 do
     docker build -f Dockerfile.20.04 -t rnakato/r_python:$tag .
     docker push rnakato/r_python:$tag
