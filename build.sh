a8d024c3eb61b75dbb0110fff7353cc8e6219831
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-08-17 21:10:16 +0900

    2022.087

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-for tag in #11.3.1-cudnn8-runtime-ubuntu20.04 latest
+for tag in 10.0-cudnn7-runtime-ubuntu18.04 #2022.08.2 latest #11.4.2-cudnn8-runtime-ubuntu20.04 #2022.08 latest #11.3.1-cudnn8-runtime-ubuntu20.04
 do
     docker build -f Dockerfile.GPU -t rnakato/r_python_gpu:$tag . #--no-cache
     docker push rnakato/r_python_gpu:$tag
 done
 
-for tag in 20.04 latest
+for tag in #2022.08.2 latest # 20.04
 do
     docker build -f Dockerfile.20.04 -t rnakato/r_python:$tag .
     docker push rnakato/r_python:$tag
--- jupyternotebook.sh
+++ jupyternotebook.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#port=$1
 jupyter notebook --allow-root --no-browser
