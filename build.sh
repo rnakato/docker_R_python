b0ea8c9301dbc702785b26397aba661aa8e77a1f
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2024-09-21 11:16:20 +0900

    Update 2024.09

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-for tag in 2024.04 latest
+for tag in 2024.09 latest
 do
-    docker build -t rnakato/r_python:$tag -f Dockerfile.micromamba --target normal .
+#    docker build -t rnakato/r_python:$tag -f Dockerfile.micromamba --target normal .
 #    docker save -o r_python-$tag.tar rnakato/r_python:$tag
 #    singularity build -F r_python.$tag.sif docker-archive://r_python-$tag.tar
 #    exit
-    docker push     rnakato/r_python:$tag
-    docker build -t rnakato/r_python_gpu:$tag --target gpu .
+#    docker push     rnakato/r_python:$tag
+    docker build -t rnakato/r_python_gpu:$tag -f Dockerfile.micromamba --target gpu .
     docker push     rnakato/r_python_gpu:$tag
 done
